b9a778d6ee588ae59b3f28019e524b5d6019d0ba
tarunsharma <sharmatarunsharma7@gmail.com>
2021-02-17 00:08:36 +0530

    added new reverse code

---
+++ code.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+
+void function(char *ptr){
+int i,j;
+
+printf("Name is %s\n",ptr);
+
+//printf("reverse of the name is\n");
+
+int len= strlen(ptr);
+
+printf("len of name is %d\n",len);
+printf("Reverse of the name is \n");
+
+for(j=len-1;j>=0;j--){
+
+printf("%c",*(ptr+j));
+
+}
+printf("\n");
+}
+
+
+void main(){
+
+char name[20];
+
+printf("Enter a name\n");
+
+scanf("%s",name);
+
+function(name);
+
+}
