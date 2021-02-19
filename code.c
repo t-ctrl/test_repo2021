b913942e6335d78c01e3548ff0da2812e0c923c6
tarunsharma <sharmatarunsharma7@gmail.com>
2021-02-19 19:42:28 +0530

    proper changes done

--- code.c
+++ code.c
@@ -29,7 +29,6 @@ printf("%c",ptr[j]-32);
 
 }
 
-<<<<<<< HEAD
 //
 
 
@@ -52,7 +51,6 @@ void main(){
 
 char name[20];
 char str1[20],str2[20];
-=======
 printf("\n");
 }
 
@@ -61,18 +59,14 @@ void main(){
 
 char name[20];
 
->>>>>>> e4ef86b0ea34e55d75352f05196cf85746e5379c
 printf("Enter a name\n");
 
 scanf("%s",name);
 
 function(name);
 
-<<<<<<< HEAD
 printf("Enter two strings\n ");
 scanf("%s %s",str1,str2);
 my_strcmp(str1,str2);
 printf("\n");
-=======
->>>>>>> e4ef86b0ea34e55d75352f05196cf85746e5379c
 }
